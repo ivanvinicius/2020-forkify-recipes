7f8c9f8ec74a78479867ba574e4865d5df4d09cd
ivanvinicius <ivanvinicius88@gmail.com>
2020-03-08 10:03:15 -0300

    feat: updating recipe servings

--- src/js/index.js
+++ src/js/index.js
@@ -83,3 +83,20 @@ const controlRecipe = async () => {
 }
 
 ['hashchange', 'load'].forEach((event) => window.addEventListener(event, controlRecipe));
+
+elements.recipe.addEventListener('click', e => {
+  if(e.target.matches('.btn-decrease, .btn-decrease * ')) {
+    if (state.recipe.servings > 1) {
+      state.recipe.updateServings('dec');
+
+      recipeView.updateServingsIngredients(state.recipe);
+    }
+  }
+  else if(e.target.matches('.btn-increase, .btn-increase * ')) {
+    if (state.recipe.servings <= 50) {
+      state.recipe.updateServings('inc'); 
+
+      recipeView.updateServingsIngredients(state.recipe);      
+    } 
+  }
+})
