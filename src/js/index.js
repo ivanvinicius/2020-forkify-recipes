20aedd46e2efb474ee6fb9ee1aaab3618bc0fe9f
ivanvinicius <ivanvinicius88@gmail.com>
2020-03-08 10:27:26 -0300

    feat: model of shopping list

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,6 @@
 import Search from './models/Search';
 import Recipe from './models/Recipe';
+import List from './models/List';
 import  { elements, renderLoader, clearLoader }  from './views/elementsDom';
 import * as searchView from './views/searchView';
 import * as recipeView from './views/recipeView';
@@ -100,3 +101,5 @@ elements.recipe.addEventListener('click', e => {
     } 
   }
 })
+
+window.l = new List();
